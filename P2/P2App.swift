0b705231d11c8248a4c139db9a09c6cacdd113c2
Noah Wilder <noahleviwilder@gmail.com>
2021-11-09 14:48:08 -0500

    Initial Commit

---
+++ P2/P2App.swift
@@ -0,0 +1,17 @@
+//
+//  P2App.swift
+//  P2
+//
+//  Created by Noah Wilder on 2021-11-09.
+//
+
+import SwiftUI
+
+@main
+struct P2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
